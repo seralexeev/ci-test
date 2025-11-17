2a097b48a9f15e989bdef18f1e73262578dd62d9
sg <ser.alexeev@gmail.com>
2025-11-17 08:22:35 +0000

    wip

--- scripts/src/ci_notify_slack.ts
+++ scripts/src/ci_notify_slack.ts
@@ -10,9 +10,33 @@ const env = z
 
 const web = new WebClient(env.SLACK_TOKEN);
 
+const md = `
+## What's Changed
+* pr 30 by @seralexeev in https://github.com/seralexeev/ci-test/pull/28
+* pr 31 by @seralexeev in https://github.com/seralexeev/ci-test/pull/29
+* pr 32 by @seralexeev in https://github.com/seralexeev/ci-test/pull/30
+* pr 33 by @seralexeev in https://github.com/seralexeev/ci-test/pull/31
+* pr 35 by @seralexeev in https://github.com/seralexeev/ci-test/pull/32
+* pr 36 by @seralexeev in https://github.com/seralexeev/ci-test/pull/33
+
+
+**Full Changelog**: https://github.com/seralexeev/ci-test/compare/web-api/release/0.0.5...tags/web-api/draft
+`;
+
 const res = await web.chat.postMessage({
-  channel: "C09TFU78Y3S",
-  text: ":sherpa-excited: CI job notification",
+  channel: "C12345678",
+  text: "Release deployed",
+  mrkdwn: true,
+  blocks: [
+    {
+      type: "header",
+      text: { type: "plain_text", text: ":sherpa-excited:" },
+    },
+    {
+      type: "section",
+      text: { type: "mrkdwn", text: md },
+    },
+  ],
 });
 
 if (res.ts == null) {
